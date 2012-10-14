33c4ff9c5ea431e750051f9f7e19d6ca329a186b
BastiaanOlij <mux213@gmail.com>
2012-10-15 07:22:01 +1100

    Merged our visual and non visual component structure into a single structure

--- example.cpp
+++ example.cpp
@@ -11,24 +11,26 @@
 #include "example.h"
 
 qint mainlib::ecm_connect(void) {
-	OXFNVobject		lvNVObject;
 	OXFcomponent	lvComponent;
 	
-	// add out object definitions here
-	lvNVObject.objectID = 2100;
-	lvNVObject.flags = 0;
-	lvNVObject.groupResID = 0;
-	lvNVObject.newObjectFunction = (void * (*)()) &(oNVExample::newObject);
-	
-	mObjects.push(&lvNVObject);
-	
 	// add our component definitions here...
+	lvComponent.componentType = cObjType_NVObject;
+	lvComponent.componentID = 2100;
+	lvComponent.bitmapID = 0;
+	lvComponent.flags = 0;
+	lvComponent.groupResID = 0;
+	lvComponent.newObjectFunction = (void * (*)()) &(oNVExample::newObject);
+	
+	addComponent(lvComponent);
+		
 	lvComponent.componentType = cObjType_Basic;
 	lvComponent.componentID = 2000;
 	lvComponent.bitmapID = 1;
+	lvComponent.flags = 0;
+	lvComponent.groupResID = 0;
 	lvComponent.newObjectFunction = (void * (*)()) &(oExample::newObject);
 		
-	mComponents.push(&lvComponent);
+	addComponent(lvComponent);
 	
 	return oXCompLib::ecm_connect();
 };
